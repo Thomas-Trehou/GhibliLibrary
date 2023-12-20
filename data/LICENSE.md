f7d06328741b8b9a8da692e3cba1655c031c0466
enzoclock <enzo.testa@oclock.io>
2023-12-20 09:38:48 +0100

    Initial commit

--- data/LICENSE.md
+++
@@ -1,3 +0,0 @@
-Data were found on this repo : https://github.com/janaipakos/ghibliapi (under MIT license)
-
-Or use the API instead : https://ghibliapi.herokuapp.com/
--- middlewares/dates.js
+++
@@ -1,9 +0,0 @@
-const dates = [
-  { firstDate: 1980, secondDate: 1989 },
-  { firstDate: 1990, secondDate: 1999 },
-  { firstDate: 2000, secondDate: 2009 },
-  { firstDate: 2010, secondDate: 2019 },
-  { firstDate: 2020, secondDate: 'today' },
-];
-
-module.exports = dates;
